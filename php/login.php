9142531bab646d2457406f84bc6229c0342dbc00
milahnmartin <66036885+milahnmartin@users.noreply.github.com>
2022-05-28 17:30:36 +0200

    feat("Added Login Functionality")

--- php/login.php
+++ php/login.php
@@ -1,6 +1,47 @@
 <?php
-if (!empty($_POST)) {
-    $first_name = $_POST['pUsername'];
+require '.env.php';
+if(isset($_POST["submit"])){
+    global $servername;
+    global $username;
+    global $database;
 
-    echo $first_name;
+    $pUsername = $_POST["pUsername"];
+    $pPassword = $_POST["pPassword"];
+
+
+    $conn = new mysqli($servername,$username,null,$database);
+
+    if($conn->connect_error){
+        die("Connection Failed");
+    }
+
+    function checkLoginUser()
+    {
+        $ExistResult = null;
+        global $pUsername, $conn,$pPassword;
+        $stmt = $conn->stmt_init();
+        $stmt->prepare("SELECT Username from account where Username = ? and Password = ?");
+        $stmt->bind_param("ss", $pUsername,$pPassword);
+        $stmt->execute();
+        $stmt->bind_result($ExistResult);
+        $stmt->fetch();
+
+        if ($ExistResult) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+
+    if(checkLoginUser()){
+        echo "SUCCESS";
+        $conn->close();
+        exit();
+    }
+
+    echo "YOU DONT EXIST";
+    $conn->close();
 }
+
